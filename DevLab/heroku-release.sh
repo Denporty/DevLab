e1e0ac2b78aee66e5a18593c1e426f027fceb35f
Victor <victor@MacBook-Pro-de-Victor.local>
2022-01-20 14:41:36 +0100

    Delete devlab file

--- DevLab/heroku-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "php artisan migrate --force"
-php artisan migrate --force
-php artisan cache:clear
-php artisan clear-compiled
-php artisan view:clear
-php artisan config:clear
-php artisan optimize:clear
