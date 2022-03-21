10ba5274e7430cea7b8d2fbf04d0c7ae49d898a8
Denporty <victor.cabot@edu.devinci.fr>
2022-03-21 10:22:30 +0100

    Try

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -12,7 +12,7 @@
                     <div class="flex flex-col ml-3">
                         <slot></slot>
                     </div>
-                    <div>Bonjour ceci est un try pour le nom</div>
+                    <div>Bonjour ceci est un try pour le nom SS-</div>
                 </div>
             </div>
         </div>
