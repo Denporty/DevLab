df284f8716c6550b6110a3919b47c87641096b10
Denporty <victor.cabot@edu.devinci.fr>
2022-07-22 13:10:55 +0200

    refacto

--- resources/js/Pages/FrontOffice/Animation/More.vue
+++ resources/js/Pages/FrontOffice/Animation/More.vue
@@ -15,8 +15,8 @@
         </div>
         <div>
             <div class="flex lg:flex-row flex-col items-end lg:justify-between mb-16">
-                <p class="inline-block bg-red-500 text-white w-fit font-bold py-2 px-4 rounded my-2">{{ findTagName(animation.tag) }}</p>
-                <p class="inline-block bg-green-500 text-white w-fit font-bold py-2 px-4 rounded my-2">{{ findDepartmentName(animation.department) }}</p>
+                <p class="inline-block bg-red-500 text-white w-fit font-bold py-2 px-4 rounded my-2">{{ findDataByName(tags ,animation.tag) }}</p>
+                <p class="inline-block bg-green-500 text-white w-fit font-bold py-2 px-4 rounded my-2">{{ findDataByName(departments ,animation.department) }}</p>
             </div>
             <img :src="animation.image" class="w-full">
             <h2 class="text-gray-800 text-5xl font-semibold capitalize">{{ animation.name }}</h2>
@@ -79,20 +79,11 @@ export default {
         }
     },
     methods: {
-        findDepartmentName (id) {
+        findDataByName (data, id) {
             let name = "";
-            this.departments?.forEach(department => {
-                if (department.id === id) {
-                    name = department.name
-                }
-            });
-            return name;
-        },
-        findTagName (id) {
-            let name = "";
-            this.tags?.forEach(tag => {
-                if (tag.id === id) {
-                    name = tag.name
+            data?.forEach(data => {
+                if (data.id === id) {
+                    name = data.name
                 }
             });
             return name;
